4990492840585490f1cb885f0368d2bca18296c2
Murat Akkus <akkusmurat123@gmail.com>
2024-12-10 17:00:57 +0100

    s

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import menuImg from "./images/menu.jpg";
+
+function App() {
+  return (
+    <div className="menu--container">
+      <img id="menuImg" src={menuImg} alt="menu" />
+    </div>
+  );
+}
+
+export default App;
